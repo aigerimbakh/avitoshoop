6562d8067a9b44ba5b765b5b96266ecebfd96ab3
aigerimbakh <milashkalovec@gmail.com>
2022-12-03 21:30:23 +0300

    git init

---
+++ src/test/java/kz/narxoz/avitoshoop/AvitoshoopApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.avitoshoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoshoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
